f6a792333377a9d30b2897139004a9ab0973f05c
BAQER <baqer.kh98@gmail.com>
2020-05-16 02:43:42 +0300

    baqer

--- config/app.php
+++ config/app.php
@@ -161,6 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        LaravelFCM\FCMServiceProvider::class,
+        Kawankoding\Fcm\FcmServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -225,6 +228,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'FCM'      => LaravelFCM\Facades\FCM::class,
+    'FCMGroup' => LaravelFCM\Facades\FCMGroup::class, // Optional
+    'Fcm' => Kawankoding\Fcm\FcmFacade::class,
+
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,62 @@
 |
 */
 
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+//use FCM;
+
 Route::get('/', 'UploadController@index');
 Route::post('/store', 'UploadController@store');
-Route::get('download/{filename}', function($filename)
-{
-    $file_path = 'files/'. $filename;
-    if (file_exists($file_path))
-    {
+Route::get('download/{filename}', function ($filename) {
+    $file_path = 'files/' . $filename;
+    if (file_exists($file_path)) {
         return Response::download($file_path, $filename, [
-            'Content-Length: '. filesize($file_path)
+            'Content-Length: ' . filesize($file_path)
         ]);
-    }
-    else
-    {
+    } else {
         exit('The file does not exist!');
     }
 })->name('download');
 Route::post('/download', 'UploadController@download');
+
+
+Route::get('/fcm2', function () {
+    $recipients = ["cyGVzrAiQ0CKW9LR_Ui0a1:APA91bEUttvARhE70o_PYtm-rH9vel3qZAm2ABuimVtB2P4RpzwMLBUuXtauG1ozjsXU7AxkBfHGOkZXcgiQ-c9UawxOStHkKHHIJcTq7urRn-qiUvwcwmg-JhCjUuZtp77Q8obEFFb6"];
+    fcm()
+        ->to($recipients) // $recipients must an array
+        ->priority('high')
+        ->timeToLive(0)
+        ->data([
+            'body' => 'testing ',
+        ])
+        ->notification([
+            'title' => 'fuck u',
+            'body' => 'اعتقد ذيج المكتبه احسن ',
+          
+        ])
+        ->send();
+
+    return "ok";
+});
+Route::get('/fcm', function () {
+    $optionBuilder = new OptionsBuilder();
+    $optionBuilder->setTimeToLive(60 * 20);
+
+    $notificationBuilder = new PayloadNotificationBuilder('Fuck you');
+    $notificationBuilder->setBody('u r bitch')
+        ->setSound('default');
+
+    $dataBuilder = new PayloadDataBuilder();
+    $dataBuilder->addData(['a_data' => 'my_data']);
+
+    $option = $optionBuilder->build();
+    $notification = $notificationBuilder->build();
+    $data = $dataBuilder->build();
+
+    $token = "cyGVzrAiQ0CKW9LR_Ui0a1:APA91bEUttvARhE70o_PYtm-rH9vel3qZAm2ABuimVtB2P4RpzwMLBUuXtauG1ozjsXU7AxkBfHGOkZXcgiQ-c9UawxOStHkKHHIJcTq7urRn-qiUvwcwmg-JhCjUuZtp77Q8obEFFb6";
+
+
+    $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+    return "ok";
+});
